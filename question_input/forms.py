49e7faa7188620a1f57411777698149543701a0d
bachdo <bachdo89@gmail.com>
2021-04-03 16:25:27 +0700

    add input_question feature

---
+++ question_input/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+
+
+class CppQuestionForm(forms.Form):
+    QUESTION_TYPES = [
+        ('TH', 'Theory'),
+        ('PR', 'Practice')
+    ]
+    question_type = forms.ChoiceField(
+        label="Question type",
+        choices=QUESTION_TYPES,
+        widget=forms.RadioSelect,
+        error_messages={
+            'required': 'Need question type',
+            'invalid_choice': 'Type %(value)s NOT exist.'
+        }
+    )
+
+    QUESTION_LEVELS = [
+        ('H', 'hard'),
+        ('M', 'medium'),
+        ('E', 'easy')
+    ]
+    question_level = forms.ChoiceField(
+        label="Question level",
+        choices=QUESTION_LEVELS,
+        widget=forms.RadioSelect,
+        error_messages={
+            'required': 'Need question level',
+            'invalid_choice': 'Level %(value)s NOT exist.'
+        }
+    )
+
+    question_content = forms.CharField(
+        label="Question content",
+        max_length=1000,
+        min_length=1,
+        error_messages={
+            'required': 'Need question content',
+            'max_length': 'At most 1000 characters',
+            'min_length': 'At least 1 characters'
+        }
+    )
+
+    question_answer = forms.CharField(
+        label="Question answer",
+        max_length=1000,
+        min_length=1,
+        error_messages={
+            'required': 'Need question answer',
+            'max_length': 'At most 1000 characters',
+            'min_length': 'At least 1 characters'
+        }
+    )
--- question_input/views.py
+++ question_input/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import CppQuestion
+from .forms import CppQuestionForm
+
 def input_question(request):
-    return render(request, "input_question.html")
+    if request.method == 'POST':
+        # create a form instance and populate it with the request's data
+        # to validate the request's data and transform them to a cleaned format
+        question_form = CppQuestionForm(request.POST)
+        if not question_form.is_valid():
+            return render(request, 'input_question_result.html', {'is_success': False})
+        
+        cleaned_form = question_form.cleaned_data
+        question = CppQuestion(
+            question_type=cleaned_form["question_type"],
+            question_level=cleaned_form["question_level"],
+            question_content=cleaned_form["question_content"],
+            question_answer=cleaned_form["question_answer"]
+        )
+        
+        # save inputted question to database
+        question.save()
+        
+        return render(request, 'input_question_result.html', {'is_success': True})
+    else:
+        question_form = CppQuestionForm()
+        return render(request, "input_question.html", {'question_form': question_form})
